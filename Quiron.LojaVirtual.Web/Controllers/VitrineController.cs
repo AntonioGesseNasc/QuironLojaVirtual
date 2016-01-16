5cf40fb634203b2a6cad740412e5869421886536
AntonioGesseNasc <antoniogessenasc@gmail.com>
2016-01-16 19:27:31 -0200

    Entity

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 4;
+        // GET: Vitrine
+        public ActionResult ListaProdutos(string categoria, int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            ProdutosViewModel model = new ProdutosViewModel
+            {
+                Produtos = _repositorio.Produtos
+                .Where(p => categoria == null || p.Categoria == categoria)
+                .OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina),
+
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = ProdutosPorPagina,
+                    ItensTotal = _repositorio.Produtos.Count()
+
+                },
+
+                CategoriaAtual = categoria
+            };
+
+            return View(model);
+        }
+    }
+}
