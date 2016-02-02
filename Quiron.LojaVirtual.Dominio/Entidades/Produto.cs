4dd029446be18fc1e20d7998e554c5a7eb94675d
AntonioGesseNasc <antoniogessenasc@gmail.com>
2016-02-02 01:14:12 -0200

    Aula 42

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -6,24 +6,28 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Produto
     {
-        [HiddenInput(DisplayValue=false)]
+        [HiddenInput(DisplayValue = false)]
         public int ProdutoId { get; set; }
-        
-        [Required(ErrorMessage="Digite o nome do produto")]
+
+        [Required(ErrorMessage = "Digite o nome do produto")]
         public string Nome { get; set; }
 
-        [Display(Name="Descrição")]
+        [Display(Name = "Descrição")]
         [DataType(DataType.MultilineText)]
         [Required(ErrorMessage = "Digite a descrição")]
         public string Descricao { get; set; }
 
-        [Display(Name="Preço")]
-        [Required(ErrorMessage="Digite o valor")]
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "Digite o valor")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Valor inválido")]
         public decimal Preco { get; set; }
 
         [Required(ErrorMessage = "Digite a categoria")]
         public string Categoria { get; set; }
+
+        public byte[] Imagem { get; set; }
+
+        public string ImagemMimeType { get; set; }
 
     }
 }
